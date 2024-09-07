b7a6309e9b82e4a0bc7f43403b275ebee02c0637
njstein19 <159060796+mrmonches@users.noreply.github.com>
2024-09-07 12:15:24 -0500

    DrinkData Getters and Setters

--- Assets/Scripts/DrinkData.cs
+++ Assets/Scripts/DrinkData.cs
@@ -3,7 +3,22 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "DrinkData", menuName = "Drinks Data")]
 public class DrinkData : ScriptableObject
 {
-    [SerializeField] private Vector3 DrinkOrders;
+    [SerializeField] private Vector3 DrinkContents;
     [SerializeField] private string DrinkName;
     [SerializeField] private Sprite DrinkSprite;
+
+    public Vector3 GetDrinkContents()
+    {
+        return DrinkContents;
+    }
+
+    public string GetDrinkName()
+    {
+        return DrinkName;
+    }
+
+    public Sprite GetDrinkSprite()
+    {
+        return DrinkSprite;
+    }
 }
